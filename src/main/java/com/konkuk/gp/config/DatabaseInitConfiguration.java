90f3fd108e712086f8ebbef4ba1dda24d247167a
jaemin <anjm1020@gmail.com>
2023-10-26 02:11:20 +0900

    test: 데이터베이스 초기화에서 할 일 추가 (#20)

--- src/main/java/com/konkuk/gp/config/DatabaseInitConfiguration.java
+++ src/main/java/com/konkuk/gp/config/DatabaseInitConfiguration.java
@@ -1,28 +1,48 @@
 package com.konkuk.gp.config;
 
+import com.konkuk.gp.domain.dao.Checklist;
+import com.konkuk.gp.domain.dao.ChecklistRepository;
+import com.konkuk.gp.domain.dao.member.MemberChecklist;
+import com.konkuk.gp.domain.dao.member.MemberChecklistRepository;
 import com.konkuk.gp.service.dialog.DialogManager;
 import com.konkuk.gp.domain.dao.member.Member;
 import com.konkuk.gp.domain.dao.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class DatabaseInitConfiguration {
     private final MemberRepository memberRepository;
+    private final ChecklistRepository checklistRepository;
+    private final MemberChecklistRepository memberChecklistRepository;
     private final DialogManager dialogManager;
 
     @PostConstruct
-    protected void init() {
+    @Transactional
+    public void init() {
         memberRepository.deleteAll();
         Member member = memberRepository.save(Member.builder()
                 .name("testMember")
                 .build());
         dialogManager.setMemberId(member.getId());
         log.info("Member : {}", member.toString());
+
+        Checklist checklist = checklistRepository.save(Checklist.builder()
+                .isComplete(false)
+                .description("cook")
+                .deadline(LocalDateTime.now().plusDays(1))
+                .build());
+        memberChecklistRepository.save(MemberChecklist.builder()
+                .checklist(checklist)
+                .member(member)
+                .build());
     }
+
 }
